4fcb93a5ad6449d7b6b787f79a6b05d9f82d87bc
wardhamansoor <wardha_21@hotmail.com>
2022-11-22 12:10:35 +0500

    changes in loginroutes

--- routes/loginroutes.js
+++ routes/loginroutes.js
@@ -9,16 +9,22 @@ logInRoute.route('/').all((req,res,next)=>{
 }).get(async (req,res)=>{
     console.log("In Login Route")
     const { username, password } = req.body
-    const customer  = await customers.findOne({ username, password })
-    if(customer){
-        //res.json(customer , msg:"");
-        res.send({msg : 'Customer found---------LOGIN SUCCESSFULL----------',
-        json : customer}
-        );
-        //res.json(customer);
+    if(username != null || password != null){
+        const customer  = await customers.findOne({ username, password })
+        if(customer){
+            //res.json(customer , msg:"");
+            res.send({msg : 'Customer found---------LOGIN SUCCESSFULL----------',
+            json : customer}
+            );
+            //res.json(customer);
+        }else{
+            res.send('Customer not found---------LOGIN FAIL----------')
+        }
     }else{
-        res.send('Customer not found---------LOGIN FAIL----------')
+        res.send('Enter username and password First')
     }
+
+    
 })
 
 export default logInRoute;
